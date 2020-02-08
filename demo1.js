5630bf66b13b02e9231407bb00a1e63bc8c48706
Chris <christopher.g.parkinson@gmail.com>
2020-02-08 14:51:55 +0000

    basically up and working but lives are weird, check all the nextClicks

--- demo1.js
+++ demo1.js
@@ -3,15 +3,34 @@
 
 function changeSquareColor(demo, color) {
     console.log("changing " + demo + " to color " + color)
+    console.log("lives="+demo1.lives)
     document.getElementById(demo).style.backgroundColor = color;
 }
 
 
 function loseLife(demo1) {
-    demo1.lives--;
+    if (demo1.lives > 0) {
+        demo1.lives--;
+        demo1.color="red";
+        demo1.timeline.clear();
+        waitToGoOrange(demo1);
+    }
+    else {
+        demo1.color="red";
+        demo1.timeline.clear();
+        demo1.timeline.add(new TimelineEvent(
+            name="death",
+            fn=function(){
+                console.log("dead")
+                demo1.reset();
+            },
+            delay=3000
+        ));
+    }
 }
 
 function success(demo1) {
+    demo1.successes++;
     demo1.color = "green";
     demo1.timeline.clear();
     waitToGoOrange(demo1);
@@ -20,14 +39,19 @@ function success(demo1) {
 function waitToGoOrange(demo1) {
     // go orange
     console.log("go orange")
-    rand_time = 1000
+    rand_time = 300 + Math.random();
     demo1.nextClick = loseLife;
     
     demo1.timeline.add(new TimelineEvent(
-        name="test123",
-        fn=function () {
+        name="go_orange",
+        fn=function (demo1) {
             demo1.color = "orange";
             demo1.nextClick = success;
+            demo1.timeline.add(new TimelineEvent(
+                name="faliure",
+                fn=loseLife,
+                delay=100+2000/(demo1.successes+1)
+            ))
         },
         delay=rand_time
     ))
@@ -52,27 +76,30 @@ class Timeline {
         this.timeline.push(elt);
     }
     refresh(demo1, delta) {
+        var toExecute = [];
+        
         this.timeline = this.timeline.filter((elt) => {
-            console.log(elt);
-            console.log("processing " + elt.name)
             elt.delay -= delta;
             if (elt.delay <= 0) {
-                // remove and execute
-                console.log("<0")
-                elt.fn(demo1)
+                toExecute.push(elt.fn);
             }
-            return (elt.delay > 0)
+            return (elt.delay > 0);
         });
+
+        toExecute.forEach(fn => fn(demo1));
     }
 }
 
 class Demo1 {
     constructor() {
         console.log("constructing")
-        this.lives = 3;
-        this.busy = false;
-        this.color = "lightgray";
         this.activecolor = "lightgray";
+        this.reset()
+    }
+    reset() {
+        this.successes = 0;
+        this.color = "lightgray";
+        this.lives = 3;
         this.nextClick = waitToGoOrange;
         this.timeline = new Timeline();
     }
@@ -94,15 +121,14 @@ function getDemo1() {
             demo1.timeline.refresh(demo1, delta);
         }
         var t = setInterval(refresh, delta);
+        demo1.refreshInterval = t;
         return demo1;
     } else {
-        console.log("already have demo1");
         return demo1;
     }
 }
 
 function clickDemo1() {
-    console.log("click received")
     demo1 = getDemo1();
     demo1.nextClick(demo1);
 }
